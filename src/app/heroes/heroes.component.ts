86171d54725997175cd9eaf20a136e6d05bf8c1b
Chris Reynoso <chrtravels@gmail.com>
2023-02-07 19:39:52 +0200

    1 through 5

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
+// heroes component presents a list of heroes
+// Parent of HeroDetailComponent
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
+  // defines a private heroService prop & identifies it as a HeroService injection site
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
 }
